20402d709dcbaaec37a64d2cc68f885edd0dcbc2
Kishore Mondi <kishoremondi2630@gmail.com>
2025-07-29 18:13:43 +0530

    Day 1 - Added Two Sum solution

---
+++ Day_01_TwoSum.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        num_map = {}  # Create a hash map to store numbers and their indices
+    
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_map:
+                return [num_map[complement], i]
+            num_map[num] = i
+        
+        return []  # In case no solution is found (though problem states there is one)
+
+        
