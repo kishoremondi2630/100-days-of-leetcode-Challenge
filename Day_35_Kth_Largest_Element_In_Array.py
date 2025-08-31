cb809438bf6ab7758f6aeeb0b5ff8892de4b9b2d
Kishore Mondi <kishoremondi2630@gmail.com>
2025-08-31 16:57:52 +0530

    Day 35 - Added Kth Largest Element in Array Solution

---
+++ Day_35_Kth_Largest_Element_In_Array.py
@@ -0,0 +1,17 @@
+import heapq
+
+class Solution(object):
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        min_heap = []
+        for num in nums:
+            if len(min_heap) < k:
+                heapq.heappush(min_heap, num)
+            else:
+                if num > min_heap[0]:
+                    heapq.heapreplace(min_heap, num)
+        return min_heap[0]
