f66890ede95a9cdc72315d6766a2118ad398e6fe
Kishore Mondi <kishoremondi2630@gmail.com>
2025-09-02 10:54:49 +0530

    Day 37 - Added Number of Islands Solution

---
+++ Day_37_Number_of_Islands.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        if not grid:
+            return 0
+        
+        count = 0
+        rows = len(grid)
+        cols = len(grid[0])
+        
+        def dfs(i, j):
+            if i < 0 or j < 0 or i >= rows or j >= cols or grid[i][j] != '1':
+                return
+            grid[i][j] = '0'
+            dfs(i+1, j)
+            dfs(i-1, j)
+            dfs(i, j+1)
+            dfs(i, j-1)
+        
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == '1':
+                    count += 1
+                    dfs(i, j)
+        return count
